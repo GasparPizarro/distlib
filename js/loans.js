039b0e9f5353a964ca5bb0e578f3249975e0e866
Gaspar Pizarro <gaspar.pizarro.v@gmail.com>
2018-12-16 22:10:50 -0300

    asdf

--- js/loans.js
+++ js/loans.js
@@ -18,36 +18,10 @@ distlib.loans = (function() {
 		for (var i = 0; i < loans.length; i = i + 1) {
 			element = document.createElement("li");
 			element.id = loans[i].id;
-			if (loans[i].status == 0) {
-				element.innerHTML = String()
-					+ '<div class="w3-container">'
-						+ '<span>' + loans[i].book.title + '</span>'
-						+ '<div class="w3-right"><button class="w3-button accept-loan"><i class="fa fa-check"></i></button><button class="w3-button reject-loan"><i class="fa fa-times"></i></button></div>'
-					+ '</div>'
-					+ '<div class="w3-container">'
-						+ loans[i].recipient
-						+ ' | '
-						+ loans[i].span + ' semanas'
-					+ '</div>';
-			}
-			else {
-				var startDate = new Date(loans[i].start);
-				var endDate = new Date(loans[i].start);
-				endDate.setDate(startDate.getDate() + loans[i].span * 7);
-				element.innerHTML = String()
-					+ '<p>'
-						+ loans[i].book.title
-						+ '<span class="w3-right">' + loans[i].recipient + '</span>'
-					+ '</p>'
-					+ '<p>'
-						+ 'Due on ' + endDate.getDate() + '-' + (endDate.getMonth() + 1) + '-' + endDate.getFullYear()
-						+ '<span class="w3-right"><button class="w3-button finish-loan"><i class="fa fa-check"></i></button></span>'
-					+ '</p>';
-			}
+			distlib.Loan(loans[i]).render(element);
 			loansList.appendChild(element);
 		}
 		document.querySelectorAll(".accept-loan").forEach(function(element){element.addEventListener("click", onAcceptLoan)});
-		document.querySelectorAll(".reject-loan").forEach(function(element){element.addEventListener("click", onRejectLoan)});
 		document.querySelectorAll(".finish-loan").forEach(function(element){element.addEventListener("click", onFinishLoan)});
 	};
 
@@ -58,51 +32,11 @@ distlib.loans = (function() {
 			clearLoans();
 			loadLoans(loans);
 		});
-	};
-
-	var onRejectLoan = function(event) {
-		event.preventDefault();
-		var loanId = event.target.closest("li").getAttribute("id")
-		distlib.services.rejectLoan(loanId).then(function() {
-			event.target.closest("li").remove();
-		});
-	};
-
-	var onAcceptLoan = function(event) {
-		event.preventDefault();
-		var loanId = event.target.closest("li").getAttribute("id");
-		distlib.services.acceptLoan(loanId).then(function(loan) {
-			event.target.closest("li").remove();
-			var startDate = new Date(loan.start);
-			var endDate = new Date(loan.start);
-			endDate.setDate(startDate.getDate() + loan.span * 7);
-			var element = document.createElement("li");
-			element.id = loan.id;
-			element.innerHTML = String()
-				+ '<p>'
-					+ loan.book.title
-					+ '<span class="w3-right">' + loan.recipient + '</span>'
-				+ '</p>'
-				+ '<p>'
-					+ 'Due on ' + endDate.getDate() + '-' + (endDate.getMonth() + 1) + '-' + endDate.getFullYear()
-					+ '<span class="w3-right"><button class="w3-button finish-loan"><i class="fa fa-check"></i></button></span>'
-				+ '</p>';
-			loansList.appendChild(element);
-		});
-
-	};
-
-	var onResolveLoan  = function(event) {
-		event.preventDefault();
-		var loanId = event.target.closest("li").getAttribute("id");
-	};
-
-	var onFinishLoan = function(event) {
-		event.preventDefault();
-		var loanId = event.target.closest("li").getAttribute("id");
-		distlib.services.finishLoan(loanId).then(function() {
-			event.target.closest("li").remove();
-		});
+		var deleteLoan = function(event) {
+			event.target.remove();
+		};
+		loansList.addEventListener("reject-loan", deleteLoan);
+		loansList.addEventListener("finish-loan", deleteLoan);
 	};
 
 	return {
